d0ddabc1be39686122c3c62b5f44ac165186a591
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2026-03-06 21:00:21 +0800

    feat: use preprocessor to emit Windows-friendly code

--- prompt.c
+++ prompt.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char *readline(char *prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char *cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy
+}
+
+/* Fake add_history function */
+void add_history(char *unused) {}
+
+/* Otherwise include the editline header */
+#else
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char **argv) {
 
@@ -19,7 +41,7 @@ int main(int argc, char **argv) {
     add_history(input);
 
     /* Echo input back to user */
-    printf("%s\n", input);
+    printf("Echoing: %s\n", input);
 
     /* Free retrieved input */
     free(input);
