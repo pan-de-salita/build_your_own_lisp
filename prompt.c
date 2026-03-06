5dd239888d17b6d1c5e9062da98d43d67b8fa063
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2026-03-06 20:44:59 +0800

    feat: use editline in prompt

--- prompt.c
+++ prompt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/readline.h>
 
 int main(int argc, char **argv) {
 
@@ -12,14 +12,17 @@ int main(int argc, char **argv) {
   /* In a never ending loop */
   while (1) {
 
-    /* Output prompt */
-    fputs("lispy> ", stdout);
+    /* Output our prompt and get input */
+    char *input = readline("lispy> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* Add input to history */
+    add_history(input);
 
-    /* kecho input back to user */
-    printf("%s", input);
+    /* Echo input back to user */
+    printf("%s\n", input);
+
+    /* Free retrieved input */
+    free(input);
   }
 
   return 0;
